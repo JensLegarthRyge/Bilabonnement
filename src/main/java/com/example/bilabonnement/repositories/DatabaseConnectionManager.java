a67ea3825157c25ed0d5185ed23e8fc5d27040b6
Johannes-Forsting <johannes.forsting@gmail.com>
2022-05-02 15:39:29 +0200

    opsætning af diverse småting

---
+++ src/main/java/com/example/bilabonnement/repositories/DatabaseConnectionManager.java
@@ -0,0 +1,18 @@
+package com.example.bilabonnement.repositories;
+
+import java.sql.*;
+
+public class DatabaseConnectionManager {
+
+    //Her connectes der til vores database med link, navn og kodeord.
+    //Noget med Singleton
+    public Connection getConnectionToDatabase(){
+        Connection con = null;
+        try {
+            //con = DriverManager.getConnection("jdbc:mysql://votum.mysql.database.azure.com:3306/votum", "rootuser@votum", "JJMdat21a");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
