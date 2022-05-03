36573bb484dd3755a8dbebdfa955f872acf8880b
MBHNielsenX <mads.nielsen12@live.dk>
2022-05-03 13:14:52 +0200

    third

--- src/main/java/com/example/bilabonnement/models/User.java
+++ src/main/java/com/example/bilabonnement/models/User.java
@@ -1,4 +1,13 @@
 package com.example.bilabonnement.models;
 
 public class User {
+
+    public static void main(String[] args) {
+        hej();
+    }
+    public static void hej() {
+        int hej = 10;
+        hej = 20;
+
+    }
 }
