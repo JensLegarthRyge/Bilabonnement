16eeea2775e230962af441ad1551f3ee0ee1b883
MBHNielsenX <mads.nielsen12@live.dk>
2022-05-04 09:59:56 +0200

    html & css 2

--- src/main/java/com/example/bilabonnement/controllers/IndexController.java
+++ src/main/java/com/example/bilabonnement/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.bilabonnement.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpSession;
 
@@ -16,5 +17,13 @@ public class IndexController {
     public String loggedInFrontpage() {
 
         return "loggedInFrontpage";
+    }
+
+    @GetMapping("/flow")
+    public String redirectToChosenSite(WebRequest dataFromForm) {
+
+        String urlToRedirect = dataFromForm.getParameter("selected-url");
+        System.out.println(urlToRedirect);
+        return "redirect:" + urlToRedirect;
     }
 }
