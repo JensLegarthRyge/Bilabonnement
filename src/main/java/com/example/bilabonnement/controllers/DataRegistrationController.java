4330cffb4da3ad01178a90f36edf69d077b463a3
JensLegarthRyge <90308706+JensLegarthRyge@users.noreply.github.com>
2022-05-09 15:28:34 +0200

    Dataregistration page + CarTestRepository.java creation

--- src/main/java/com/example/bilabonnement/controllers/DataRegistrationController.java
+++ src/main/java/com/example/bilabonnement/controllers/DataRegistrationController.java
@@ -1,15 +1,27 @@
 package com.example.bilabonnement.controllers;
 
+import com.example.bilabonnement.models.Car;
+import com.example.bilabonnement.repositories.CarTestRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 
 @Controller
 public class DataRegistrationController {
 
     @GetMapping("/data-registration")
-    public String dataRegistration() {
+    public String dataRegistration(HttpSession session, Model carModel) {
+        //TEST REPLACE FOR PROD
+        CarTestRepository ctr = new CarTestRepository();
+        ArrayList<Car> allCars = ctr.getAll();
+
+        carModel.addAttribute("allCars",allCars);
+
         return "data-registration";
     }
 
@@ -18,4 +30,6 @@ public class DataRegistrationController {
         System.out.println(uploadRequest.getParameter("registration-file"));
         return "redirect:/data-registration";
     }
+
+
 }
---
+++ src/main/java/com/example/bilabonnement/repositories/CarTestRepository.java
@@ -0,0 +1,54 @@
+package com.example.bilabonnement.repositories;
+
+import com.example.bilabonnement.models.Car;
+import com.example.bilabonnement.models.Enum.EnergyLabel;
+import com.example.bilabonnement.models.Enum.FuelType;
+import com.example.bilabonnement.repositories.interfaces.CRUDInterface;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+//Jens Legarth Ryge
+
+public class CarTestRepository implements CRUDInterface<Car> {
+    ArrayList<Car> allCars = new ArrayList<>(
+            Arrays.asList(
+                    new Car(1, "BS23576", "YSK7759123J1723", false, true, FuelType.DIESEL,
+                            17.4, EnergyLabel.D, "JETBLACK", 144699,  109, 1.3),
+                    new Car(1, "UX89717", "JLKJD1892731JKASD", true, true, FuelType.PETROL,
+                            20.8, EnergyLabel.B, "CORALBLUE", 89599,  76, 1.4),
+                    new Car(1, "AL86337", "YSK7759123J1723", false, false, FuelType.ELECTRIC,
+                            11, EnergyLabel.A, "ANTHRACITEGRAY", 699095,  170, 2.7)
+            )
+    );
+
+    @Override
+    public boolean create(Car entity) {
+        return false;
+    }
+
+    @Override
+    public ArrayList<Car> getAll() {
+        return allCars;
+    }
+
+    @Override
+    public Car getSingleById(int id) {
+        for (Car cr:allCars) {
+            if (cr.getId()==id){
+                return cr;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public boolean update(Car entity) {
+        return false;
+    }
+
+    @Override
+    public void delete(int id) {
+
+    }
+}
