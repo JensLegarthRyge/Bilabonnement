4f88f5f6f6c8064c75520213185ceefdc3228047
JohannesForsting98 <johannes.forsting@gmail.com>
2022-05-22 22:07:21 +0200

    Man kan oprette nye medarbejdere nu

--- src/main/java/com/example/bilabonnement/controllers/AdminController.java
+++ src/main/java/com/example/bilabonnement/controllers/AdminController.java
@@ -1,11 +1,15 @@
 package com.example.bilabonnement.controllers;
 
 import com.example.bilabonnement.repositories.EmployeeRepository;
+import com.example.bilabonnement.services.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
+
+import java.net.http.HttpRequest;
 
 @Controller
 public class AdminController {
@@ -17,9 +21,17 @@ public class AdminController {
     }
 
 
+    @PostMapping("create-new-employee-site")
+    public String createNewEmployeeSite(){
+
+
+        return "new-employee";
+    }
+
     @PostMapping("create-new-employee")
-    public String createNewEmployee(){
-        System.out.println("in create new employee");
+    public String createNewEmployee(WebRequest dataFromForm){
+        EmployeeService empService = new EmployeeService();
+        empService.createNewEmployee(dataFromForm);
 
         return "redirect:/admin";
     }
--- src/main/java/com/example/bilabonnement/services/EmployeeService.java
+++ src/main/java/com/example/bilabonnement/services/EmployeeService.java
@@ -1,4 +1,30 @@
 package com.example.bilabonnement.services;
 
+import com.example.bilabonnement.models.Employee;
+import com.example.bilabonnement.repositories.EmployeeRepository;
+import org.springframework.web.context.request.WebRequest;
+
+import java.net.http.HttpRequest;
+
 public class EmployeeService {
+
+    public void createNewEmployee(WebRequest dataFromForm){
+        EmployeeRepository er = new EmployeeRepository();
+        try{
+            String firstName = dataFromForm.getParameter("firstName");
+            String lastName = dataFromForm.getParameter("lastName");
+            String password = dataFromForm.getParameter("password");
+            int accessRights = Integer.parseInt(dataFromForm.getParameter("accessRights"));
+            boolean isAdmin = dataFromForm.getParameter("isAdmin") != null ? true : false;
+            String email = dataFromForm.getParameter("email");
+
+            er.create(new Employee(firstName, lastName, password, accessRights, isAdmin, email));
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Employee not created");
+        }
+
+    }
 }
