bfc4c95c4fe1bc1f562b7aa37b2a439cb0a2e69d
MBHNielsenX <mads.nielsen12@live.dk>
2022-05-24 16:48:18 +0200

    Edit page

--- src/main/java/com/example/bilabonnement/controllers/DataRegistrationController.java
+++ src/main/java/com/example/bilabonnement/controllers/DataRegistrationController.java
@@ -4,8 +4,6 @@ import com.example.bilabonnement.models.LeaseReport;
 import com.example.bilabonnement.repositories.*;
 import com.example.bilabonnement.services.LeaseReportService;
 import com.example.bilabonnement.services.LoginService;
-import com.example.bilabonnement.services.ManualUpload;
-
 import com.example.bilabonnement.services.CSVFileService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -61,9 +59,8 @@ public class DataRegistrationController {
     }
 
     @PostMapping("/edit-lease-update")
-    public String updateLease(WebRequest dataFromForm, @ModelAttribute("updateLease") LeaseReport report) {
     public String updateLease(WebRequest dataFromForm) {
-        int id = Integer.parseInt(dataFromForm.getParameter("lease-id"));
+        int id = Integer.parseInt(dataFromForm.getParameter("edit-lease-id"));
         int carId = Integer.parseInt(dataFromForm.getParameter("car-chassis"));
         int customerId = Integer.parseInt(dataFromForm.getParameter("edit-customer-id"));
         int employeeId = Integer.parseInt(dataFromForm.getParameter("employee-id"));
@@ -74,8 +71,11 @@ public class DataRegistrationController {
         LocalDate startDate = LocalDate.parse(dataFromForm.getParameter("start-date"));
         LocalDate createdDate = LocalDate.parse(dataFromForm.getParameter("edit-created-date"));
         int period = Integer.parseInt(dataFromForm.getParameter("edit-period"));
+        double price = Double.parseDouble(dataFromForm.getParameter("edit.price"));
 
+        LeaseReport leaseReport = new LeaseReport(id,carId,employeeId,customerId,createdDate,period,hasReturnInsurance,hasLowDeductable,pickupLocationId,isLimited,price,startDate);
 
+        System.out.println(leaseReport);
 
         return "redirect:/data-registration";
     }
