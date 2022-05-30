71efc05960b5e55c743a03aecd3d28022d0cefa4
MBHNielsenX <mads.nielsen12@live.dk>
2022-05-30 08:35:37 +0200

    Overhaul

--- src/main/java/com/example/bilabonnement/controllers/DamageReportController.java
+++ src/main/java/com/example/bilabonnement/controllers/DamageReportController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.WebRequest;
-
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 
--- src/main/java/com/example/bilabonnement/services/ModelService.java
+++ src/main/java/com/example/bilabonnement/services/ModelService.java
@@ -1,15 +1,17 @@
 package com.example.bilabonnement.services;
+import com.example.bilabonnement.models.LeaseReport;
 import com.example.bilabonnement.repositories.*;
 import org.springframework.ui.Model;
 
+// Mads Bøgh Højer Nielsen
 public class ModelService {
-    static CarRepository carRepo = new CarRepository();
-    static LeaseReportRepository lrr = new LeaseReportRepository();
-    static CustomerRepository cr = new CustomerRepository();
-    static EmployeeRepository er = new EmployeeRepository();
-    static PickupLocationRepository plr = new PickupLocationRepository();
+    CarRepository carRepo = new CarRepository();
+    LeaseReportRepository lrr = new LeaseReportRepository();
+    CustomerRepository cr = new CustomerRepository();
+    EmployeeRepository er = new EmployeeRepository();
+    PickupLocationRepository plr = new PickupLocationRepository();
 
-    public static Model fillModel(Model model) {
+    public Model fillModel(Model model) {
         model.addAttribute("pickupLocationRepository", plr);
         model.addAttribute("employeeRepository",er);
         model.addAttribute("customerRepository",cr);
@@ -19,6 +21,45 @@ public class ModelService {
         model.addAttribute("allLeaseReports",lrr.getAll());
         model.addAttribute("allPickupLocations",plr.getAll());
         model.addAttribute("allEmployees", er.getAll());
+        return model;
+    }
+    public Model insertLease(Model model, int id) {
+        LeaseReport lr = lrr.getSingleById(id);
+        int carId = lr.getCarId();
+        int customerId = lr.getCustomerId();
+        int employeeId = lr.getEmployeeId();
+        int pickupLocationId = lr.getPickupLocationId();
+        model.addAttribute("id", id);
+        model.addAttribute("carId", carId);
+        model.addAttribute("customerId", customerId);
+        model.addAttribute("employeeId", employeeId);
+        model.addAttribute("pickupLocationId", pickupLocationId);
+        model.addAttribute("carChassis", lr.getCarById(carId).getChassisNumber());
+        model.addAttribute("customerNameAndId", lr.getCustomerById(customerId).getIdPlusFullName());
+        model.addAttribute("employeeNameAndId", lr.getEmployeeById(employeeId).getIdPlusFullName());
+        model.addAttribute("hasReturnInsurance", lr.hasReturnInsurance());
+        if (lr.hasReturnInsurance() == true) {
+            model.addAttribute("textHasReturnInsurance", "Har afleveringsforsikring");
+        } else if (lr.hasReturnInsurance() == false){
+            model.addAttribute("textHasReturnInsurance", "Har ikke afleveringsforsikring");
+        }
+        model.addAttribute("hasLowDeductableInsurance", lr.hasLowDeductableInsurance());
+        if (lr.hasLowDeductableInsurance() == true) {
+            model.addAttribute("textHasLowDeductable", "Har selvrisiko forsikring");
+        } else if (lr.hasLowDeductableInsurance() == false) {
+            model.addAttribute("textHasLowDeductable", "Har ikke selvrisiko forsiking");
+        }
+        model.addAttribute("isLimited", lr.isLimited());
+        if (lr.isLimited() == true) {
+            model.addAttribute("textIsLimited", "Limited");
+        } else if (lr.isLimited() == false) {
+            model.addAttribute("textIsLimited", "Unlimited");
+        }
+        model.addAttribute("getPickupLocation", lr.getPickupLocationById(pickupLocationId).getStreetNameNumberPostcode());
+        model.addAttribute("startDate", lr.getStartDate());
+        model.addAttribute("createdDate", lr.getCreatedDate());
+        model.addAttribute("period", lr.getPeriod());
+
         return model;
     }
 }
